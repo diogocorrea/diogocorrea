4687701ef246b46148f5147f3896078e6ae2f341
Diogo Correa <diogodca@gmail.com>
2019-12-19 13:01:54 -0300

    offline

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
 self.addEventListener("install", event => {
-	console.log(event);
+	event.waitUntil(
+		caches.open("airhorner").then(cache => {
+			return cache.addAll(["/", "/index.html", "/montserrat.woff2"]);
+		})
+	);
+});
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			return response || fetch(event.request);
+		})
+	);
 });
