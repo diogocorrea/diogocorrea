55cfd25609f5d5cef88861e5660f75870001139f
Diogo Correa <diogodca@gmail.com>
2021-03-11 18:58:19 -0300

    clear sw cache

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
 	event.waitUntil(
 		caches
-			.open("diogocorrea_v2")
-			.then(cache =>
+			.open("diogocorrea_v3")
+			.then((cache) =>
 				cache.addAll(["/", "/index.html", "/montserrat.woff2"])
 			)
 	);
 });
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
 	event.respondWith(
 		caches
 			.match(event.request)
-			.then(response => response || fetch(event.request))
+			.then((response) => response || fetch(event.request))
 	);
 });
